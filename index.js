70343828f75ab6c302e0073e07c25fe26159dfd8
kwfelix <kwfelix@hotmail.com>
2014-12-16 16:45:38 +0800

    my wechat Cloud Code
    wechat cloud code based on node.js

---
+++ index.js
@@ -0,0 +1,15 @@
+/**
+ * Created by felix on 14-12-5.
+ */
+var server = require("./server");
+var router = require("./router");
+var requestHandlers = require("./requestHandlers");
+
+var handle = {}
+handle["/"] = requestHandlers.start;
+handle["/start"] = requestHandlers.start;
+handle["/upload"] = requestHandlers.upload;
+handle["/show"] = requestHandlers.show;
+
+server.start(router.route, handle);
+
---
+++ server.js
@@ -0,0 +1,18 @@
+/**
+ * Created by felix on 14-12-5.
+ */
+var http = require("http");
+var url = require("url");
+
+function start(route, handle) {
+    function onRequest(request, response) {
+        var pathname = url.parse(request.url).pathname;
+        console.log("Request for " + pathname + " received.");
+        route(handle, pathname, response, request);
+    }
+
+    http.createServer(onRequest).listen(8888);
+    console.log("Server has started.");
+}
+
+exports.start = start;
